ae35c597d9613151ae607fd3e932234e0aa5c572
kazia3 <kazia3@mcmaster.ca>
2023-03-05 21:14:29 -0500

    adding client functionality to main file

--- server.py
+++ server.py
@@ -10,6 +10,7 @@ class Server:
     recvsize = 1024
     encoding = 'utf-8'
     backlog = 10
+    grades = []
 
     def __init__(self):
         self.readcsv()
@@ -20,10 +21,14 @@ class Server:
     def readcsv(self):
 
         file = open('./course_grades_2023.csv')
+        reader = csv.reader(file)
 
-        self.rawdata = [clean_line for clean_line in
-                [line.strip() for line in file.readlines()]
-                if clean_line != '']
+        for i in reader:
+            self.grades.append(i)
+
+        # self.rawdata = [clean_line for clean_line in
+        #         [line.strip() for line in file.readlines()]
+        #         if clean_line != '']
 
         file.close()
 
@@ -84,3 +89,117 @@ class Server:
                 break
     
 
+########################################################################
+# Echo Client class
+########################################################################
+
+class Client:
+
+    SERVER_HOSTNAME = socket.gethostname()
+
+
+    # RECV_BUFFER_SIZE = 5 # Used for recv.    
+    RECV_BUFFER_SIZE = 1024 # Used for recv.
+
+    def __init__(self):
+        self.get_socket()
+        self.connect_to_server()
+        self.send_console_input_forever()
+
+    def get_socket(self):
+        try:
+            # Create an IPv4 TCP socket.
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+            # Allow us to bind to the same port right away.            
+            # self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            # Bind the client socket to a particular address/port.
+            # self.socket.bind((Client.HOSTNAME, 40000))
+                
+        except Exception as msg:
+            print(msg)
+            sys.exit(1)
+
+    def connect_to_server(self):
+        try:
+            # Connect to the server using its socket address tuple.
+            self.socket.connect((Client.SERVER_HOSTNAME, Server.PORT))
+            print("Connected to \"{}\" on port {}".format(Client.SERVER_HOSTNAME, Server.PORT))
+        except Exception as msg:
+            print(msg)
+            sys.exit(1)
+
+    def get_console_input(self):
+        # In this version we keep prompting the user until a non-blank
+        # line is entered, i.e., ignore blank lines.
+        while True:
+            self.input_text = input("Input: ")
+            if self.input_text != "":
+                break
+    
+    def send_console_input_forever(self):
+        while True:
+            try:
+                self.get_console_input()
+                self.connection_send()
+                self.connection_receive()
+            except (KeyboardInterrupt, EOFError):
+                print()
+                print("Closing server connection ...")
+                # If we get and error or keyboard interrupt, make sure
+                # that we close the socket.
+                self.socket.close()
+                sys.exit(1)
+                
+    def connection_send(self):
+        try:
+            # Send string objects over the connection. The string must
+            # be encoded into bytes objects first.
+            self.socket.sendall(self.input_text.encode(Server.MSG_ENCODING))
+        except Exception as msg:
+            print(msg)
+            sys.exit(1)
+
+    def connection_receive(self):
+        try:
+            # Receive and print out text. The received bytes objects
+            # must be decoded into string objects.
+            recvd_bytes = self.socket.recv(Client.RECV_BUFFER_SIZE)
+
+            # recv will block if nothing is available. If we receive
+            # zero bytes, the connection has been closed from the
+            # other end. In that case, close the connection on this
+            # end and exit.
+            if len(recvd_bytes) == 0:
+                print("Closing server connection ... ")
+                self.socket.close()
+                sys.exit(1)
+
+            print("Received: ", recvd_bytes.decode(Server.MSG_ENCODING))
+
+        except Exception as msg:
+            print(msg)
+            sys.exit(1)
+
+########################################################################
+# Process command line arguments if this module is run directly.
+########################################################################
+
+# When the python interpreter runs this module directly (rather than
+# importing it into another file) it sets the __name__ variable to a
+# value of "__main__". If this file is imported from another module,
+# then __name__ will be set to that module's name.
+
+if __name__ == '__main__':
+    roles = {'client': Client,'server': Server}
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-r', '--role',
+                        choices=roles, 
+                        help='server or client role',
+                        required=True, type=str)
+
+    args = parser.parse_args()
+    roles[args.role]()
+
+########################################################################
